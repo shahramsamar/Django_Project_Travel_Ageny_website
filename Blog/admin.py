b86cf6877a7ef320cd1175ccab9b352d6580a5b1
shahram <shahramsamar2010@gmail.com>
2024-01-06 15:05:03 +0330

    update admin
    update admin:
    +list_display
    + list_filter
    + ordering
    + search_fields

--- Blog/admin.py
+++ Blog/admin.py
@@ -4,6 +4,11 @@ from Blog.models import Post
 
 #@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
-
+   date_hierarchy = "create_date"
+   #empty_value_display = '-empty-'
+   list_display =('title', 'counted_views', 'status', 'published', 'create_date',)
+   list_filter =("status",)
+   ordering =['-create_date']
+   search_fields =['title','content']
+   
 admin.site.register(Post, PostAdmin)
