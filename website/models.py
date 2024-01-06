54a95a7a6170ecbb1bb5bb583499416c8d4ec4eb
shahram <shahramsamar2010@gmail.com>
2024-01-06 12:22:13 +0330

    create models in my project
    create model and db in my project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)# 2024-01-06 07:17:32.429784
+                                                      # 2024-01-06 07:20:15.293047
+                                                      
+# Select * from  post
+# select * from post where status = 1
