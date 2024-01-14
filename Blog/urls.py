e76c953b7775966baf28ea9e9d84e88a61fd65e8
shahram <shahramsamar2010@gmail.com>
2024-01-14 22:58:34 +0330

    update render

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('urls address,'view',name)
     path('', blog_view, name='index'),
     path('single', blog_single, name="single"),
+    path('test', test, name='test'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from Blog.models import Post
 
 def blog_view(requests):
     return render(requests,'blog/blog-home.html')
 
 def blog_single(requests):
     return render(requests,'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.all()
+    # posts = Post.objects.filter(status =1)
+    context = {'posts':posts}
+    return render(request,'test.html', context )
