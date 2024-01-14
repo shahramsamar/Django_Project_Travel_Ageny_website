79d81dab1fcef49f24c6000b83bddd13e4b8d597
shahram <shahramsamar2010@gmail.com>
2024-01-14 20:17:08 +0330

    connect to render

--- Blog/views.py
+++ Blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from Blog.models import Post
 
 def blog_view(requests):
-    return render(requests,'blog/blog-home.html')
+    posts = Post.objects.filter(status =1)
+    context = {'posts':posts}
+    return render(requests,'blog/blog-home.html', context)
 
 def blog_single(requests):
     return render(requests,'blog/blog-single.html')
 
 def test(request):
     posts = Post.objects.all()
-    # posts = Post.objects.filter(status =1)
-    context = {'posts':posts}
     return render(request,'test.html', context )
