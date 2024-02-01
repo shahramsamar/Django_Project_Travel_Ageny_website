f4d951535f6390a6541084abf582e04ae085a06f
shahram <shahramsamar2010@gmail.com>
2024-02-01 15:11:30 +0330

    counted_views by click in post

--- Blog/views.py
+++ Blog/views.py
@@ -4,26 +4,37 @@ from django.utils import timezone
 
 
 
-def counted_views(requests, post_id):
-    blog_object = Post.objects.get(id=post_id)
-    blog_object.counted_views += 1
-    blog_object.save()
-    return blog_object
+# def counted_views(requests, post_id):
+#     blog_object = Post.objects.get(id=post_id)
+#     blog_object.counted_views += 1
+#     blog_object.save()
+#     return blog_object
 
 
 def blog_view(requests):
     # date_time = timezone.now()
     # posts = Post.objects.filter(published_date__lte = date_time)
+    # view_count = Post.objects.get(id=pid)
+    # view_count.counted_views += 1
+    # view_count.save()
     posts = Post.objects.filter(status=1)
     context = {'posts': posts}
     return render(requests, 'blog/blog-home.html', context)
 
 
 
-def blog_single(requests,pid):
+def blog_single(requests, pid):
+    view_count = Post.objects.get(id=pid)
+    view_count.counted_views += 1
+    view_count.save()
     post = get_object_or_404(Post, pk=pid)
-    context = {'post': post}
+    posts = Post.objects.filter(status=1)
+    context = {'post': post ,'posts': posts}
     return render(requests, 'blog/blog-single.html', context)
+
+
+
+
 
 
 def test(request, pid):
