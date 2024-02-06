b03680c99348608bee92ba5b6bb4e47780b31293
shahram <shahramsamar2010@gmail.com>
2024-02-06 08:56:18 +0330

    edith my view in blog

--- Blog/views.py
+++ Blog/views.py
@@ -7,19 +7,17 @@ from django.utils import timezone
 
 def blog_view(requests):
     date_time = timezone.now()
-    posts = Post.objects.filter(status=1) & Post.objects.filter(published_date__lte=date_time)
-    # post = get_object_or_404(Post, pk=pid)
+    posts = Post.objects.filter(status=1,published_date__lte=date_time)
     context = {'posts': posts}
     return render(requests, 'blog/blog-home.html', context)
 
 
 
 def blog_single(requests, pid):
-    view_count = Post.objects.get(id=pid)
-    view_count.counted_views += 1
-    view_count.save()
     date_time = timezone.now()
-    post = get_object_or_404(Post, pk=pid) or Post.objects.filter(status=1 ) & Post.objects.filter(published_date__lte=date_time)
+    post = get_object_or_404(Post, pk=pid, status=1, published_date__lte=date_time)
+    post.counted_views += 1
+    post.save()
     context = {'post':post }
     return render(requests, 'blog/blog-single.html', context)
 
