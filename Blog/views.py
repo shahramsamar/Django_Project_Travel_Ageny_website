70ba6f913d04c9913854a60ffb2868be748914ea
shahram <shahramsamar2010@gmail.com>
2024-02-11 11:41:48 +0330

    add next post and previous post

--- Blog/views.py
+++ Blog/views.py
@@ -16,9 +16,12 @@ def blog_view(requests):
 def blog_single(requests, pid):
     date_time = timezone.now()
     post = get_object_or_404(Post, pk=pid, status=1, published_date__lte=date_time)
+    # current_posts =Post.objects.get(pk =pid)
+    related_posts = Post.objects.all()
     post.counted_views += 1
     post.save()
-    context = {'post':post }
+    context = {'post':post , 'next':related_posts.filter(id__gt=post.id).order_by('id').first(),
+               'previous':related_posts.filter(id__lt=post.id).order_by('-id').first() }
     return render(requests, 'blog/blog-single.html', context)
 
 
