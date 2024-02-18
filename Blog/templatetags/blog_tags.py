0392d1e29d51cc4f4bb6071954b950f72d529a02
shahram <shahramsamar2010@gmail.com>
2024-02-18 20:34:03 +0330

    added blog-post-category and lastet-post and fix error my page up and page down

--- Blog/templatetags/blog_tags.py
+++ Blog/templatetags/blog_tags.py
@@ -1,9 +1,10 @@
 
 from django import template
-from Blog.models import Post
+from Blog.models import Post, Category
 
 
 register = template.Library()
+
 
 @register.simple_tag(name='totalposts')
 def function():
@@ -15,3 +16,24 @@ def function():
 def function():
     posts = Post.objects.filter(status=1)
     return posts
+
+
+@register.filter
+def snippet(value, arg=20):
+    return value[:arg] + "..."
+
+
+@register.inclusion_tag('blog/blog-latest-post.html')
+def latest_posts():
+    post = Post.objects.filter(status=1).order_by('published_date')[:3]
+    return {'post': post}
+
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def post_category():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories': cat_dict}
