132097235bcfcfde179e0baa75f653820c97fdea
shahram <shahramsamar2010@gmail.com>
2024-02-19 11:24:23 +0330

    added link by cagegoryname and aouthorname

--- Blog/views.py
+++ Blog/views.py
@@ -3,33 +3,35 @@ from Blog.models import Post
 from django.utils import timezone
 
 
-
-
-def blog_view(request):
+def blog_view(request, **kwargs):
     date_time = timezone.now()
     posts = Post.objects.filter(status=1, published_date__lte=date_time)
+    if kwargs.get('cat_name')!=None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 
-
 def blog_single(request, pid):
     date_time = timezone.now()
-    post = get_object_or_404(Post, pk=pid, status=1, published_date__lte=date_time)
-    related_posts = Post.objects.filter(status=1, published_date__lte=date_time)
+    post = get_object_or_404(Post, pk=pid, status=1,
+                             published_date__lte=date_time)
+    related_posts = Post.objects.filter(
+        status=1, published_date__lte=date_time)
     post.counted_views += 1
     post.save()
-    context = {'post':post , 'next':related_posts.filter(id__gt=post.id).order_by('id').first(),
-               'previous':related_posts.filter(id__lt=post.id).order_by('-id').first() }
+    context = {'post': post, 'next': related_posts.filter(id__gt=post.id).order_by('id').first(),
+               'previous': related_posts.filter(id__lt=post.id).order_by('-id').first()}
     return render(request, 'blog/blog-single.html', context)
+
 
 def blog_category(request, cat_name):
     posts = Post.objects.filter(status=1)
     posts = posts.filter(category__name=cat_name)
-    context = {'posts':posts}
-    return render(request,'blog/blog-home.html', context )
-    
-    
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 
 def test(request):
