ae99f191352ddc9d2f107c452480ceaaf86b3d02
shahramsamar <shahramsamar2010@gmail.com>
2024-05-08 15:33:49 +0330

    Delete blog directory

--- blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from blog.models import Comment
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['post','name', 'email', 'subject', 'message']  
-
-
