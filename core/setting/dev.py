f5e6f016787b74e1b7fa327afa3e6c305fd80783
shahram <shahramsamar2010@gmail.com>
2024-05-28 15:13:36 +0330

    Revert "change setting"
    This reverts commit ed2dddd916e0f442436a1f07b0704b4a645e1d98.

--- core/setting/dev.py
+++ core/setting/dev.py
@@ -19,11 +19,18 @@ SITE_ID = 2
 
 
 
-INSTALLED_APPS = [
-     'debug_toolbar',
-]
+# INSTALLED_APPS = [
+# ]
 
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 if DEBUG:
     DATABASES = {
         'default': {
@@ -48,11 +55,11 @@ else:
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# STATIC_ROOT = BASE_DIR / 'static'
+# MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [
     BASE_DIR / "statics"
 ]
-
-
 # compressor
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'blog',
     'accounts',
     'robots',
+    'debug_toolbar',
     'taggit',
     'tinymce',
     'captcha',
