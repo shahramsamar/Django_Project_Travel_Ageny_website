543bfba90dfd8dc42493c4b5d7e7270282567b6d
shahramsamar <shahramsamar2010@gmail.com>
2024-05-30 11:24:47 +0330

    added liarajson and nginex.conf

--- core/settings.py
+++ core/settings.py
@@ -56,18 +56,7 @@ AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
 # EMAIL_HOST_USER = 'Shahramsamar2010@gmail.com'
 # EMAIL_HOST_PASSWORD = 'xdqd vrql xdwb qxnl'
 
-# if DEBUG:
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# else:
-# # email configs
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     EMAIL_USE_TLS = True
-#     EMAIL_USE_SSL = True
-#     MAIL_PORT = 587
-#     EMAIL_HOST = 'smtp.gmail.com'
-#     EMAIL_HOST_USER = 'Shahramsamar2010@gmail.com'
-#     EMAIL_HOST_PASSWORD = 'xdqd vrql xdwb qxnl'
-# DEFAULT_FROM_EMAIL ='Shahramsamar2010@gmail.com'
+
 
 
 #captcha admin setting
