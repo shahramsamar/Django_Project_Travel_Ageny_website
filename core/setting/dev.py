06acf89f9b4dff333c02528d513cc8630ada060a
shahramsamar <shahramsamar2010@gmail.com>
2024-06-03 17:15:59 +0330

    updated

--- core/setting/dev.py
+++ core/setting/dev.py
@@ -1,4 +1,5 @@
 from core.settings import * 
+from decouple import config
 
 
 
@@ -48,12 +49,12 @@ if DEBUG:
 else:
     DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': "postgresql",
-        'USER': "postgresql",
-        'PASSWORD': "0000",
-        'HOST': "127.0.0.1",
-        'PORT': "5432",
+        'ENGINE': config("DB_ENGINE"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD':config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT",cast=int),
     }
 }
 
@@ -88,3 +89,25 @@ COMPRESS_JS_FILTERS = [
 
 # Cache settings for compressor
 COMPRESS_CACHE_BACKEND = 'default'
+
+
+# security config for production
+# deploy 
+if config("USE_SSL_SETTINGS",cast=bool, default=False):
+    # Https settings
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    
+    # HTTPS SETTINGS
+    SECURE_HSTS_SECONDS = 315360000
+    SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+    # MORE SECURITY SETTING
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'SAMEORIGIN'
+    SECURE_REFERRER_POLICY = "strict-origin"
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
--- core/setting/prod.py
+++ core/setting/prod.py
@@ -5,21 +5,21 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY',default='django-insecure-t8_r3fao0u3x)f(x13t0soizooczzxh*&=5mbm=_v2qz&-#%h1')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False,cast=bool)
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND =config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST',)
-EMAIL_PORT = config('EMAIL_PORT',cast=int)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL',cast=bool)
+EMAIL_BACKEND =config('EMAIL_BACKEND',default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST',default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT',cast=int,default='587')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD',default='xdqd vrql xdwb qxnl')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER',default='Shahramsamar2010@gmail.com')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool,default='True')
+EMAIL_USE_SSL = config('EMAIL_USE_SSL',cast=bool,default='True')
 
 
 
@@ -43,12 +43,12 @@ SITE_ID = 2
 
 DATABASES = {
     'default': {
-        'ENGINE': config("DB_ENGINE"),
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD':config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT",cast=int),
+        'ENGINE': config("DB_ENGINE",default='django.db.backends.postgresql'),
+        'NAME': config("DB_NAME",default='postgres'),
+        'USER': config("DB_USER",default='postgres'),
+        'PASSWORD':config("DB_PASSWORD",default='0000'),
+        'HOST': config("DB_HOST",default='127.0.0.1'),
+        'PORT': config("DB_PORT",cast=int,default='5432'),
     }
 }
 
@@ -59,10 +59,10 @@ DATABASES = {
 # STATIC_ROOT = BASE_DIR / 'static'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
-    BASE_DIR / "statics"
+    BASE_DIR / "static",
 ]
 
 # compressor
@@ -87,34 +87,23 @@ COMPRESS_JS_FILTERS = [
 # Cache settings for compressor
 COMPRESS_CACHE_BACKEND = 'default'
 
+# security config for production
 # deploy 
+# if config("USE_SSL_SETTINGS",cast=bool, default=False):
+ # Https settings
 CSRF_COOKIE_SECURE = True
-
-## X-XSS-Protection
-SECURE_BROWSER_XSS_FILTER = True
-
-# ## X-Frame-Options
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-# #X-Content-Type-Options
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# ## Strict-Transport-Security
-# SECURE_HSTS_SECONDS = 15768000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-
-# ## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
-# SECURE_SSL_REDIRECT = True 
-
-# # for more security
-# CSRF_USE_SESSIONS = True
-# CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
-# SESSION_COOKIE_SAMESITE = 'Strict'
+SECURE_SSL_REDIRECT = True
+    
+# HTTPS SETTINGS
+SECURE_HSTS_SECONDS = 315360000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# SECURE_BROWSER_XSS_FILTER = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# CSRF_TRUSTED_ORIGINS = ['127.0.0.1']
+# MORE SECURITY SETTING
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SECURE_REFERRER_POLICY = "strict-origin"
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
